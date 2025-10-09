0577288971f7047b99048b94f17be327b6600c1c
ROHIT19K <rohitk26311@outlook.com>
2025-10-09 12:20:16 +0530

    EDA And Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,7 @@ import sys
 import logging 
 import os 
 from datetime import datetime 
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -19,6 +20,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-
-    
